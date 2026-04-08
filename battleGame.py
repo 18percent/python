c7f0d2b7e029bd72f6ae003cf59b5114b565720d
almus <rizky.almus@gmail.com>
2026-04-09 01:24:02 +0700

    pp

---
+++ battleGame.py
@@ -0,0 +1,42 @@
+import random
+
+player1 = 100
+musuh   = 100
+
+while True:
+    print (" HP kamu: ", player1)
+    print (" HP musuh: ", musuh)
+    
+    print ("1. Serang")
+    print ("2. Heal")
+    print ("0. Kabur")
+
+    aksi = input ("pilih aksi: ")
+
+    if aksi == "0":
+        print ("yah kabur takut")
+        break
+    if aksi not in ["1", "2"]:
+        print ("error")
+        continue
+    if aksi == "1":
+        damage = random.randint (10, 20)
+        musuh -= damage
+        print ("damage ", damage, "HP musuh", musuh)
+    if aksi == "2":
+        heal = random.randint (5, 15)
+        player1 += heal 
+        print ("heal sebanyak ", heal, "HP kamu ", player1)
+    
+    if musuh <= 0 :
+        print ("kamu menang")
+        break
+
+    damage_musuh = random.randint (10, 20)
+    player1 -= damage_musuh
+    print ("Damage diterima", damage_musuh, "sisa HP ", player1)
+    
+    if player1 <= 0:
+        print("Kamu Kalah")
+        break
+
---
+++ main.py
@@ -0,0 +1,39 @@
+# print ("hello world")
+
+while True:
+
+
+    print ("pilih operasi")
+    print ("1. tambah ")
+    print ("2. kurang ")
+    print ("3. bagi ")
+    print ("4. kali ")
+    print ("0. keluar")
+
+    pilihan = input("masukkan pilihan anda 1/2/3/4/0 ")
+    if pilihan == "0":
+        print ("logout")
+        break
+    
+    a = int (input ("masukkan angka pertama: "))
+    b = int (input ("masukkan angka kedua: "))
+
+    if pilihan == "1":
+        hasil = (a + b)
+    elif pilihan == "2":
+        hasil = (a - b)
+    elif pilihan == "3":
+        if b == 0:
+            print ("error")
+            continue
+        hasil = (a / b)
+    elif pilihan == "4":
+        hasil = (a * b)
+    else:
+        hasil = ("tidak memilih")
+
+    print (hasil)
+
+
+
+    
