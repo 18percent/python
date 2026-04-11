915421e17f384fdc050c03a58b414f9968ed5f46
almus <rizky.almus@gmail.com>
2026-04-11 20:50:10 +0700

    sesi 2 cuy array nii

--- cuypy.py
+++ cuypy.py
@@ -1,18 +1,26 @@
 import random
 
 welcome_message = "Selamat Datang"
+cuypy_posistion = random.randint (1, 4)
 
 print("********************")
 print(f"** {welcome_message} **")
 print("********************")
 
 nama_player = input("masukkan nama anda: ")
-while True:
-    cuypy_posistion = random.randint (1, 4)
 
+bentuk_goa = "|_|"
+goa_kosong = [bentuk_goa] * 4 # BENTUK GOA YANG TIDAK BOLEH DI UBAH 
+
+goa = goa_kosong.copy() # variabel copy an yang berisi aray goa kosong 4 biji dan agar dapat di ubah 
+goa[cuypy_posistion - 1] = "0_0" # merubah index goa ke posisi cuypy yg sdh random dengan 0_0
+
+
+while True:
+    
     print (f''' 
     halo {nama_player} coba tebak di antara 4 goa ini dimana cuypy berada
-    |_| |_| |_| |_| 
+    {goa_kosong}
     ''')
 
     while True:
@@ -22,22 +30,22 @@ while True:
             print("error")
             continue
 
-        konfirmasi_player = input(f"yakin pilih {pilihan_player}? yes/no: ")
+        konfirmasi_player = input(f"yakin pilih {pilihan_player}? y/n: ")
 
-        if konfirmasi_player == "no":
+        if konfirmasi_player == "n":
             print("pilih ulang")
             continue
             
-        if konfirmasi_player == "yes":
+        if konfirmasi_player == "y":
             if pilihan_player == cuypy_posistion:
-                print("KAMU MENANG!!!")
+                print(f" {goa} KAMU MENANG!!!")
                 break
             else: 
-                print(f"KAMU SALAH tebak lagi: ")
+                print(f" KAMU SALAH tebak lagi: ")
                 continue
             
-    ulang = input("main lagi ? yes/no ")
+    ulang = input("main lagi ? y/n ")
 
-    if ulang == "no":
+    if ulang == "n":
         print("logout")
         break
